2ae7c96fd02077df7b34cfa36967fbf635f28fd3
António <oantoniomauricio@gmail.com>
2023-01-19 10:50:31 +0000

    data02 organized

--- scripts.js
+++ scripts.js
@@ -25,8 +25,26 @@ Promise.all([
     d3.json('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json')
 ]).then(function ([data01, data02]) {
 
-    console.log(data01)
-    console.log(data02)
+    // Let's get all the ids in data01
+    idsArray = [];
+
+    for (let x = 0; x < data01.objects.counties.geometries.length; x++) {
+        idsArray.push(data01.objects.counties.geometries[x].id)
+    }
+
+    // Let's order the ids at data02 to match the counties order at data01
+    organizedData02 = [];
+
+    for (let x = 0; x < idsArray.length; x++) {
+        for (let y = 0; y < data02.length; y++) {
+            if (idsArray[x] === data02[y].fips) {
+                organizedData02.push(data02[y])
+            }
+        }
+    }
+
+    console.log(idsArray)
+    console.log(organizedData02)
 
     // Draw the map
     svg.append("g")
@@ -41,5 +59,8 @@ Promise.all([
         .attr("data-fips", (d) => d.id)
         .attr("fill", "grey")
         .style("stroke", "none")
+        .data(organizedData02)
+        .attr("data-education", (d) => d.bachelorsOrHigher)
+
 
 })
